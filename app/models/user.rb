d37fe535f9e0c0a935573dcfc8e5986e63ba6d53
Tracer923 <r.arisaka13@gmail.com>
2023-05-05 13:06:23 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, presence: true
@@ -13,8 +19,18 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
+    
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
   
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
   
+  def following?(user)
+    followings.include?(user)
+  end
   
   def get_profile_image(width, height)
     unless profile_image.attached?
