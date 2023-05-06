533f5309febd3f2180658005b47dcb35714fddd3
Tracer923 <r.arisaka13@gmail.com>
2023-05-06 12:54:17 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
 
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
